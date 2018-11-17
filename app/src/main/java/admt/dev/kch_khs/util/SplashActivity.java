6d278cfe1d0fa4fd748ef030ecfd9f34dcf12917
khaiz18 <khaizlett@gmail.com>
2018-11-17 14:06:58 -0500

    added SplashActivity

---
+++ app/src/main/java/admt/dev/kch_khs/util/SplashActivity.java
@@ -0,0 +1,16 @@
+package admt.dev.kch_khs.util;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(admt.dev.kch_khs.R.layout.activity_splash);
+
+
+    }
+}
